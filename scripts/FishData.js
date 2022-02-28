bf1974d04fc18d605608589d64669526d1484c6b
Houston-Smith <houstonbsmith@gmail.com>
2022-02-28 13:25:52 -0600

    Initial Commit

---
+++ scripts/FishData.js
@@ -0,0 +1,40 @@
+const fishCollection = [
+  {
+    species: "Yellow Tang",
+    length: 15,
+    location: "Honolulu, HI",
+    saltwater: true,
+    image: "bluefish.jpg",
+    name: "Frodo"
+  },
+  {
+    species: "Sword Fish",
+    length: 2.5,
+    location: "Florida",
+    saltwater: true,
+    image: "/images/swordFish.jpg",
+    name: "Sharpy"
+  },
+  {
+    species: "Shark",
+    length: 25,
+    location: "Florida",
+    saltwater: true,
+    image: "shark.jpg",
+    name: "Bruce"
+  },
+]
+
+
+export const getFish = () => {
+  return fishCollection
+}
+
+export const addFish = () => {
+
+}
+
+export const eatFish = () => {
+
+}
+
